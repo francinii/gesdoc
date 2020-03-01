261462521e743fa0eefa01fcbcc6e950dca2f894
francinii <francini113@gmail.com>
2020-03-01 11:06:08 -0600

    Cambios en la tabla de roles fronend

--- resources/views/rols/index.blade.php
+++ resources/views/rols/index.blade.php
@@ -8,25 +8,22 @@
 @section('title', 'Roles')
 @section('header')
 @include('layouts.header') 
-
-
 @section('content')
 <div class="container-fluid">
    <div class="row  justify-content-center">         
-        <h2 class="text-center">Listado de roles del sistema</h2>             
+        <h2 class="text-center">Roles del sistema</h2>                      
    </div>    
-   <div class="row  justify-content-center">              
-       <div class="justify-content-center "> 
-        <div class="col-md-12 text-right">
-            <button data-toggle="modal" class=" float-right btn btn-success" onclick="clearDescription()" data-target="#create">Agregar</button>
-            <br> 
-        </div>  
- 
-        @include('rols.table')
-       </div>
-     
-   </div>
- 
+   <div class="row  justify-content-center"> 
+        <div class="col-md-8 text-right">
+            <button data-toggle="modal" class=" float-right btn btn-success" onclick="clearDescription()" data-target="#create">
+                <i class="fas fa-plus-circle"></i>   Agregar
+            </button>    
+        </div>
+        <div  class="col-md-12">&nbsp</div>
+        <div class="col-md-8">
+            @include('rols.table')
+        </div>
+    </div> 
 </div>
 
 @include('rols.create')
--- resources/views/rols/table.blade.php
+++ resources/views/rols/table.blade.php
@@ -1,23 +1,35 @@
 <table id='table' class="table table-responsive table-striped">
             <thead class="thead-dark">
-                <tr>
-                    <th>Id</th>
-                    <th >Descripcion del Rol</th>
-                    <th>Modificar Rol</th>
-                    <th>Eliminar Rol</th>
+                <tr class="">
+                    <th class="col-1 text-center">Id</th>
+                    <th class="col-5 text-center">Descripcion del Rol</th>
+                    <th class="col-2 text-center">Usuarios asociados</th>
+                    <th class="col-2 text-center">Modificar Rol</th>
+                    <th class="col-2 text-center">Eliminar Rol</th>
                 </tr>
             </thead>
             <tbody >
                 @foreach ($rols as $rol)
                 <tr>
-                    <td class="">{{$rol->id}}</td>
-                    <td class="">{{$rol->description}}</td>
-                    <td class="">
-                            <button onclick = "edit('{{$rol->id}}', '{{$rol->description}}' )"  class="btn btn-info"  data-toggle="modal" class=" float-right btn btn-success " >Editar</button>
+                    <td class="col-1 text-center">{{$rol->id}}</td>
+                    <td class="col-5 text-center">{{$rol->description}}</td>
+                    <td class="col-2 text-center">         
+                         <button onclick = "edit('{{$rol->id}}', '{{$rol->description}}' )"  class="btn btn-info"  data-toggle="modal">
+                            <i class="fas fa-user">
+                            </i>
+                        </button>
                     </td>
-                    <td class="">
+                    <td class="col-2 text-center">
+                            <button onclick = "edit('{{$rol->id}}', '{{$rol->description}}' )"  class="btn btn-success"  data-toggle="modal" >
+                                <i class="fas fa-edit"></i>
+                            </button>
+                    </td>
+                    <td class="col-2 text-center">
                         <form method="POST" action="{{url('/rols/'.$rol->id)}}">
-                            <button type="button" onclick="ajaxDelete({{$rol->id}})"  class=" btn btn-danger">Eliminar</button>
+                            <button type="button" onclick="ajaxDelete({{$rol->id}})"  class=" btn btn-danger">
+                                <i class="fas fa-trash-alt">
+                                </i>
+                            </button>
                         </form>
                     </td>
                 </tr>
