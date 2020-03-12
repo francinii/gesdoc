384822339cbe8edbb061173c76edd5fb6d6ac49c
dannyvr <dannyvalerioramirez@gmail.com>
2020-03-11 21:07:52 -0600

    Danny

--- database/migrations/2020_02_17_010427_create_rols_table.php
+++ database/migrations/2020_02_17_010427_create_rols_table.php
@@ -18,6 +18,7 @@ class CreateRolsTable extends Migration
             $table->bigIncrements('id');
             $table->string('description',500);
             $table->timestamps();
+            $table->onDelete('cascade');
             
         });
     }
