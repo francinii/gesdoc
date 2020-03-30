da53b29a713b65e0b721339322b19a11ca4a2447
dannyvr <dannyvalerioramirez@gmail.com>
2020-03-29 21:14:45 -0600

    pruebas de bootstrap

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -2,12 +2,14 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8"  name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" >    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title> Gesdoc - @yield('title') </title>
-    <link rel="stylesheet"  href="{{ asset('../resources/extencions/fontawesome-free-5.13.0-web/css/all.css') }}" >
-    <link rel="stylesheet"   href="{{ asset('../resources/extencions/Bootstrap-4-4.1.1/css/bootstrap.min.css') }}" >
     <link rel="stylesheet"    href="css/estilos.css">
+    <link rel="stylesheet"  href="{{ asset('../resources/extencions/fontawesome-free-5.13.0-web/css/all.css') }}" >   
+    <link rel="stylesheet"   href="{{ asset('../resources/extencions/Bootstrap-4-4.1.1/css/bootstrap.min.css') }}" >    
     <link rel="stylesheet"   href="{{ asset('../resources/extencions/DataTables-1.10.20/css/jquery.dataTables.min.css') }}">
+   
 
     <script type="text/javascript"  src = "{{ asset('../resources/extencions/JQuery-3.3.1/jquery-3.3.1.min.js') }}"></script>
     <script type="text/javascript"  src="{{ asset('../resources/extencions/Bootstrap-4-4.1.1/js/bootstrap.min.js') }}"></script>
--- resources/views/roles/table.blade.php
+++ resources/views/roles/table.blade.php
@@ -1,20 +1,20 @@
-<table id='table' class="table table-responsive table-striped">
+<table id='table' class="table table-responsive table-striped" cellspacing="0" width="100%">
             <thead class="thead-dark">
                 <tr class="">
-                    <th class="col-lg-1 text-center">{{ __('app.roles.table.id') }}</th>
-                    <th class="col-lg-7 text-center">{{ __('app.roles.table.description') }}</th>
-                    <th class="col-lg-1 text-center">{{ __('app.roles.table.permission') }}</th>
-                    <th class="col-lg-1 text-center">{{ __('app.roles.table.user') }}</th>
-                    <th class="col-lg-1 text-center">{{ __('app.roles.table.edit') }}</th>
-                    <th class="col-lg-1 text-center">{{ __('app.roles.table.delete') }}</th>
+                    <th class="col-2 text-center">{{ __('app.roles.table.id') }}</th>
+                    <th class="col-6 text-center">{{ __('app.roles.table.description') }}</th>
+                    <th class="col-1 text-center">{{ __('app.roles.table.permission') }}</th>
+                    <th class="col-1 text-center">{{ __('app.roles.table.user') }}</th>
+                    <th class="col-1 text-center">{{ __('app.roles.table.edit') }}</th>
+                    <th class="col-1 text-center">{{ __('app.roles.table.delete') }}</th>
                 </tr>
             </thead>
             <tbody >
                 @foreach ($roles as $role)         
                 <tr class="">
-                    <td class=" col-lg-1  text-center">{{$role->id}}</td>
-                    <td class=" col-lg-7  text-center">{{$role->description}}</td>
-                    <td class=" col-lg-1  text-center"> 
+                    <td class="   text-center">{{$role->id}}</td>
+                    <td class="  text-center">{{$role->description}}</td>
+                    <td class="  text-center"> 
                         
                         
                         <?php
@@ -24,24 +24,23 @@
                           //   echo $rolp->users;
                         ?>
 
-                    
                         <button onclick = "list({{ $permissionsAsociados}}, '{{$role->description}}',1)"  class="btn btn-primary"  data-toggle="modal">
                            <i class="fas fa-lock-open">
                            </i>
                        </button>
                    </td>
-                    <td class=" col-lg-1  text-center">         
+                    <td class=" text-center">         
                          <button onclick = "list({{$role->users}}, '{{$role->description}}',2 )"  class="btn btn-info"  data-toggle="modal">
                             <i class="fas fa-user">
                             </i>
                         </button>
                     </td>
-                    <td class=" col-lg-1  text-center">
+                    <td class="text-center">
                             <button onclick = "edit('{{$role->id}}', '{{$role->description}}', {{$permissions}},{{ $permissionsAsociados}} )"  class="btn btn-success"  data-toggle="modal" >
                                 <i class="fas fa-edit"></i>
                             </button>
                     </td>
-                    <td class=" col-lg-1  text-center">
+                    <td class=" text-center">
                         <form method="POST" action="{{url('/roles/'.$role->id)}}">
                             <button type="button" onclick="confirmDelete({{$role->id}} ,'roles','table', '¿Desea eliminar el rol: {{$role->description}}?')"  class=" btn btn-danger">
                                 <i class="fas fa-trash-alt">
