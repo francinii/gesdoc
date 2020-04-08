13f08e59f1fa28f5cae5f4337e791d728326ca70
dannyvr <dannyvalerioramirez@gmail.com>
2020-04-07 21:43:14 -0600

    Probando CKEDITOR5

--- resources/views/textEditor/textEditor.blade.php
+++ resources/views/textEditor/textEditor.blade.php
@@ -3,8 +3,7 @@
 @section('title', 'Editor')
 @section('head')
 <link rel="stylesheet"  href="{{ asset('../resources/css/textEditor.css') }}" >
-<script src="https://cdn.ckeditor.com/4.8.0/full-all/ckeditor.js"></script>
-
+<script src="https://cdn.ckeditor.com/ckeditor5/18.0.0/decoupled-document/ckeditor.js"></script>
 
 <script src="{{ asset('../resources/js/textEditor.js') }}" defer></script>
 <script src="{{ asset('../resources/js/sharedFunctions.js') }}" defer></script>
@@ -16,9 +15,14 @@
 <div class="container-fluid">
    <div class="row  justify-content-center">
         <div class="col-md-10">
-            <textarea id="editor1">
-           
-            </textarea>
+                <div class="document-editor">
+                    <div class="document-editor__toolbar"></div>
+                    <div class="document-editor__editable-container">
+                    <div class="document-editor__editable">
+                        <p>The initial editor data.</p>
+                    </div>
+                </div>
+        </div>
         </div>
     </div>
 </div>
