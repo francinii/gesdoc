2ddd4399d96bde614aae45fac75ccf41c22cc2ec
francinii <francini113@gmail.com>
2020-08-14 18:18:04 -0600

    pruebas wopi

--- resources/views/documents/wopihost.blade.php
+++ resources/views/documents/wopihost.blade.php
@@ -8,11 +8,11 @@
 @section('content')
 <div id="#mainContainer" style="width:100%; height:85vh;">
 
-	<form id="loleafletform" name="loleafletform"  target="loleafletframe" action="{{ env('APP_URL') }}/loleaflet/dist/loleaflet.html?WOPISrc={{ env('APP_URL') }}/gesdoc/public/wopi/files/{{$documet}}" method="post">
-	<!--<form id="loleafletform" name="loleafletform"  target="loleafletframe" action="{{ env('APP_URL') }}/gesdoc/public/wopi/files/{{$documet}}/contents" method="post">	-->
+<form id="loleafletform" name="loleafletform"  target="loleafletframe" action="{{ env('APP_URL') }}/loleaflet/dist/loleaflet.html?WOPISrc={{ env('APP_URL') }}/gesdoc/public/wopi/files/{{$documet}}" method="post"> 
+<!--	<form id="loleafletform" name="loleafletform"  target="loleafletframe" action="{{ env('APP_URL') }}/gesdoc/public/wopi/files/{{$documet}}" method="get">   -->
 		 <input name="access_token" value="{{ $api_token}}"  type="hidden"/>
 	</form>
-	
+
  <iframe id="loleafletframe" name= "loleafletframe"   allowfullscreen style="width:100%; height:100%;"></iframe>
 </div>
 <script>
